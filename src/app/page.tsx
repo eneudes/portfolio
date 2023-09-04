cf42d87ba02618de07b29a0769e154d55c44aaa8
eneudes <eneudes.ap@gmail.com>
2023-09-04 12:25:48 -0300

    creating-page-estructure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,51 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+    <main>
+      <header>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/vercel.svg"
+          alt="foto perfil"
+          width={100}
+          height={24}
           priority
         />
-      </div>
+        <div>
+          <h1>Hi, i´m Eneudes! 👋</h1>
+          <h2>Software Developer</h2>
+        </div>
+      </header>
+      <div className="experience">
+        <h3>Experience</h3>
+        <p>
+          More than 3 years working as a Software Developer, always
+          professionalizing the search for new challenges...
+        </p>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <div className="experiece-time"></div>
+        <div className="infos">
+          <h3>Lenguages</h3>
+          <div className="languages-info">
+            <span> EN basic reading</span>
+            <span> PT-BR Native Speaker</span>
+          </div>
+          <h3>Education</h3>
+          <div className="education-info">
+            <span>🎓</span>
+            <span>
+              {" "}
+              Technologist in Internet Systems - Faculty of Technology META -
+              Macapá-AP
+            </span>
+          </div>
+        </div>
+        <div className="buttons">
+          <div className="social">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          </div>
+          <button>Contact Me</button>
+        </div>
       </div>
     </main>
-  )
+  );
 }
