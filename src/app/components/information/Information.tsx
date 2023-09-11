ffe558b87e6147cfb14f9a0567c519880917225b
eneudes <eneudes.ap@gmail.com>
2023-09-11 12:43:51 -0300

    app

---
+++ src/app/components/information/Information.tsx
@@ -0,0 +1,30 @@
+import SectionTitle from "../sectionTitle/SectionTitle";
+import Image from "next/image";
+import "./information.scss";
+
+export default function Info() {
+  return (
+    <div className="info">
+      <SectionTitle text="Lenguages" />
+      <div className="lenguages-info">
+        <div className="lengue-icon">
+          <Image src="/eua.png" alt="" width={25} height={25} />
+          <span> EN basic reading</span>
+        </div>
+        <div className="lengue-icon">
+          <Image src="/br.png" alt="" width={26} height={26} />
+          <span> PT-BR Native Speaker</span>
+        </div>
+      </div>
+      <SectionTitle text="Education" />
+      <div className="education-info">
+        <span>🎓</span>
+        <span>
+          {" "}
+          Technologist in Internet Systems - Faculty of Technology META -
+          Macapá-AP
+        </span>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,17 @@
 import Experience from "./components/experience/Experience";
 import Header from "./components/header/Header";
+import EmailIcon from "./components/icons/EmailIcon";
+import Information from "./components/information/Information";
 
 export default function Home() {
   return (
     <main className="container">
       <Header />
       <Experience />
-      <div className="infos">
-        <h3>Lenguages</h3>
-        <div className="languages-info">
-          <span> EN basic reading</span>
-          <span> PT-BR Native Speaker</span>
-        </div>
-        <h3>Education</h3>
-        <div className="education-info">
-          
-          <span>🎓</span>
-          <span>
-            {" "}
-            Technologist in Internet Systems - Faculty of Technology META -
-            Macapá-AP
-          </span>
-        </div>
-      </div>
+      <Information/>
       <div className="buttons">
         <div className="social"></div>
-        <button>Contact Me</button>
+        <button className="btn-primary">Contact Me <EmailIcon/></button>
       </div>
     </main>
   );
